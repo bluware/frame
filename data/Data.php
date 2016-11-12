811feb468c48f506dd2b6086bae09cea36ab0c06
emelnychenko <eugen.melnychenko@gmail.com>
2016-11-12 18:22:00 +0200

    struct data root class

---
+++ data/Data.php
@@ -0,0 +1,17 @@
+<?php
+
+/**
+ *  Blu PHP Lite & Scaleable Web Frame
+ *
+ *  @package  Blu
+ *  @author   Eugen Melnychenko
+ */
+namespace Blu;
+
+/**
+ * @subpackage Data
+ */
+abstract class Data implements ControllerInterface
+{
+    //
+}
