67b24b20eb6c5c0771810027c9cf9f1483a3c07f
emelnychenko <eugen.melnychenko@gmail.com>
2017-01-03 14:27:53 +0200

    Bit implementation

--- bit/Bit.php
+++ bit/Bit.php
@@ -8,35 +8,49 @@
  */
 namespace Frame;
 
+use Frame\Data;
+
 /**
- * @subpackage Aspect
+ * @subpackage Bit
  */
 class Bit implements BitInterface
 {
     /**
      *  @var array
      */
-    protected $assoc = [];
+    protected $maps = [];
 
     /**
      *  @var array
      */
-    protected $mask  = [];
-
-    /**
-     *  @var array
-     */
-    protected $input = 0;
+    protected $mask  = 0;
 
     /**
      *  @return mixed
      */
-    public function __construct($input, array $assoc = null)
+    public function __construct($mask, array $maps = null)
     {
         /**
          *  @var integer
          */
-        $this->input = $input;
+        $this->mask = $mask;
+
+        /**
+         *  @var array
+         */
+        $maps = $maps === null ?
+            $this->maps : $maps;
+
+        /**
+         *  @var Data
+         */
+        $this->maps = new Data();
+
+        /**
+         *  @var bool
+         */
+        if (sizeof($maps) === 0)
+            return;
 
         /**
          *  @var integer
@@ -46,39 +60,179 @@ class Bit implements BitInterface
         /**
          *  @var iterable
          */
-        do {
+        foreach ($maps as $name) {
             /**
-             *  @var bool
+             *  @var integer
              */
-            if ($input & $index) {
-                $input -= $index;
+            $this->maps->set(
+                $name, $index
+            );
 
-
-            }
-
+            /**
+             *  @var integer
+             */
             $index += $index;
-        } while (
-            $input !== 0
-        );
+        }
     }
 
     public function get($bit)
     {
-        return $this->input & $bit;
+        /**
+         *  @var bool
+         */
+        return boolval(
+            $this->mask & $this->maps->get(
+                $bit, $bit
+            )
+        );
     }
 
+    /**
+     *  @param integer $bit
+     *  @param integer $value
+     */
     public function set($bit, $value)
     {
-        $isset = $this->input & $bit;
+        $bit  = $this->maps->get(
+            $bit, $bit
+        );
 
+        /**
+         *  @var bool
+         */
+        $isset = $this->get($bit);
+
+        /**
+         *  @var integer
+         */
         $value = boolval($value);
 
+        /**
+         *  @var bool
+         */
         if ($isset && !$value)
-            $this->input -= $bit;
+            $this->mask -= $bit;
 
+        /**
+         *  @var bool
+         */
         if (!$isset && $value)
-            $this->input += $bit;
+            $this->mask += $bit;
 
+        /**
+         *  @var $this
+         */
         return $this;
+    }
+
+    public function mask($mask = null)
+    {
+        /**
+         *  @var bool
+         */
+        if ($mask === null)
+            /**
+             *  @var integer
+             */
+            return $this->mask;
+
+        /**
+         *  @var bool
+         */
+        if ($mask !== '*') {
+            /**
+             *  @var integer
+             */
+            $this->mask = $mask;
+
+            /**
+             *  @var $this
+             */
+            return $this;
+        }
+
+        /**
+         *  @var integer
+         */
+        $this->mask = $this->bit('*');
+
+        /**
+         *  @var $this
+         */
+        return $this;
+    }
+
+    public function bit($bits)
+    {
+        /**
+         *  @var bool
+         */
+        if ($bits === '*')
+            /**
+             *  @var array
+             */
+            $bits = array_values(
+                $this->maps->data()
+            );
+
+        /**
+         *  @var bool
+         */
+        if (gettype($bits) !== 'array')
+            /**
+             *  @var array
+             */
+            $bits = func_get_args();
+
+        /**
+         *  @var integer
+         */
+        $mask = 0;
+
+        /**
+         *  @var iterable
+         */
+        foreach ($bits as $bit)
+            /**
+             *  @var integer
+             */
+            $mask += $this->maps->get(
+                $bit, $bit
+            );
+
+        /**
+         *  @var integer
+         */
+        return $mask;
+    }
+
+    /**
+     *  @param  scalar $bit
+     *
+     *  @return bool
+     */
+    public function __get($bit)
+    {
+        return $this->get($bit);
+    }
+
+    /**
+     *  @param  scalar $bit
+     *
+     *  @return bool
+     */
+    public function __set($bit, $value)
+    {
+        return $this->set($bit, $value);
+    }
+
+    /**
+     *  @param  scalar $bit
+     *
+     *  @return bool
+     */
+    public function __isset($bit)
+    {
+        return $this->get($bit);
     }
 }
---
+++ bit/BitInterface.php
@@ -0,0 +1,51 @@
+<?php
+
+/**
+ *  Bluware PHP Lite & Scaleable Web Frame
+ *
+ *  @package  Frame
+ *  @author   Eugen Melnychenko
+ */
+namespace Frame;
+
+/**
+ * @subpackage Bit
+ */
+interface BitInterface
+{
+    /**
+     *  @return mixed
+     */
+    public function __construct($mask, array $maps = null);
+
+    public function get($bit);
+
+    public function mask($mask = null);
+
+    /**
+     *  @param integer $bit
+     *  @param integer $value
+     */
+    public function set($bit, $value);
+
+    /**
+     *  @param  scalar $bit
+     *
+     *  @return bool
+     */
+    public function __get($bit);
+
+    /**
+     *  @param  scalar $bit
+     *
+     *  @return bool
+     */
+    public function __set($bit, $value);
+
+    /**
+     *  @param  scalar $bit
+     *
+     *  @return bool
+     */
+    public function __isset($bit);
+}
