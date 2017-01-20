81b975ba41ba95ab608abf38e2817c5d2b7c2a34
emelnychenko <eugen.melnychenko@gmail.com>
2017-01-20 18:59:24 +0200

    Hook Controller support

---
+++ hook/Hook/Controller.php
@@ -0,0 +1,44 @@
+<?php
+
+/**
+ *  Bluware PHP Lite Web & API Framework
+ *
+ *  @package  Frame
+ *  @author   Eugen Melnychenko
+ */
+namespace Frame\Hook;
+
+use Frame\App;
+
+/**
+ * @subpackage Controller
+ */
+abstract class Controller extends \Frame\Controller
+{
+    /**
+     *  @var array
+     */
+    protected $events = [];
+
+    /**
+     *  @param App $app
+     */
+    public function __construct(App $app)
+    {
+        /**
+         *  @var void
+         */
+        parent::__construct($app);
+
+        /**
+         *  @var iterable
+         */
+        foreach ($this->events as $event => $method)
+            /**
+             *  @var void
+             */
+            $this->hook('event', $event, [
+                $this, $method
+            ]);
+    }
+}
