72e8890e3dd132d428540b224d6008ac8913df5f
emelnychenko <eugen.melnychenko@gmail.com>
2017-06-11 21:02:36 +0300

    Autoload cache fix

--- autoload/Autoload.php
+++ autoload/Autoload.php
@@ -87,7 +87,7 @@ class Autoload
         }
 
         $export = sprintf(
-            "<?php\n\nreturn %s;", var_export($this->classmap->getData(), true)
+            "<?php\n\nreturn %s;\n", var_export($this->classmap->getData(), true)
         );
 
         file_put_contents($filepath, $export);
